fa56455d82071945d7f61fa7e803215fab452c09
Ishaan-11 <ishaansharma1998@gmail.com>
2021-01-04 14:05:51 +0530

    add: Display component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Display from "./Display";
 import "./App.css";
 
 function App() {
   return (
     <div className="component-app">
-      <h1>Hello</h1>
+      <Display />
     </div>
   );
 }
---
+++ src/components/Display.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Display.css";
+
+function Display() {
+  return (
+    <div className="component-display">
+      <div>display</div>
+    </div>
+  );
+}
+
+export default Display;
