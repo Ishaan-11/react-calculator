fa7491572272ad043b73af570440db1f6f9081b9
Ishaan-11 <ishaansharma1998@gmail.com>
2021-01-04 17:23:43 +0530

    add: logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,25 @@
 import React, { useState } from "react";
 import Display from "./Display";
 import ButtonPanel from "./ButtonPanel";
+import calculate from "../logic/calculate";
 import "./App.css";
 
 function App() {
-  const [total, setTotal] = useState("0");
+  const [data, setData] = useState({
+    total: null,
+    next: null,
+    operation: null
+  });
 
   function handleClick(buttonName) {
-    setTotal(buttonName);
+    setData(prevValue => {
+      return calculate(prevValue, buttonName);
+    });
   }
 
   return (
     <div className="component-app">
-      <Display value={total} />
+      <Display value={data.next || data.total || "0"} />
       <ButtonPanel clickHandle={handleClick} />
     </div>
   );
